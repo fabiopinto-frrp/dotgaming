729ea93792f440e831d4d21f0913ecf0d9640228
fabiopinto-frrp <fabiopinto.epbjc@gmail.com>
2024-08-02 15:10:59 +0100

    feat: Service worker to cache the project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,8 +4,27 @@ import About from "./Pages/About/About";
 import Portfolio from "./Pages/Portfolio/Portfolio";
 import Contact from "./Pages/ContactUs/Contact";
 import "./App.css";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      window.addEventListener("load", () => {
+        navigator.serviceWorker.register("/service-worker.js").then(
+          (registration) => {
+            console.log(
+              "ServiceWorker registration successful with scope: ",
+              registration.scope
+            );
+          },
+          (error) => {
+            console.log("ServiceWorker registration failed: ", error);
+          }
+        );
+      });
+    }
+  }, []);
+
   return (
     <>
       <BrowserRouter>
