465872e1a712941d3f75eb045c345bd4e847fa36
fabiopinto-frrp <fabiopinto.epbjc@gmail.com>
2024-08-16 11:48:37 +0100

    feat: Created SectionsData with objects to call in other Pages / components

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -3,38 +3,10 @@ import { Helmet, HelmetProvider } from "react-helmet-async";
 import Navbar from "../../Components/Navbar/Navbar";
 import HeroSection from "../../Components/Section/HeroSection";
 import PrimarySection from "../../Components/Section/PrimarySection";
-import testIcon from "../../Assets/favicon.png";
+import items from "../../SectionsData/Servicesitems";
 import { HomeBody } from "./HomeElements";
 
 const Home = () => {
-  const items = [
-    {
-      icon: { testIcon },
-      subTitle: "Sub Title 1",
-      content: "Content 1",
-    },
-    {
-      icon: { testIcon },
-      subTitle: "Sub Title 2",
-      content: "Content 2",
-    },
-    {
-      icon: { testIcon },
-      subTitle: "Sub Title 3",
-      content: "Content 3",
-    },
-    {
-      icon: { testIcon },
-      subTitle: "Sub Title 4",
-      content: "Content 4",
-    },
-    {
-      icon: { testIcon },
-      subTitle: "Sub Title 5",
-      content: "Content 5",
-    },
-  ];
-
   return (
     <>
       <HelmetProvider>
---
+++ frontend/src/SectionsData/Servicesitems.jsx
@@ -0,0 +1,31 @@
+import testIcon from "../Assets/favicon.png";
+
+const items = [
+  {
+    icon: { testIcon },
+    subTitle: "Sub Title 1",
+    content: "Content 1",
+  },
+  {
+    icon: { testIcon },
+    subTitle: "Sub Title 2",
+    content: "Content 2",
+  },
+  {
+    icon: { testIcon },
+    subTitle: "Sub Title 3",
+    content: "Content 3",
+  },
+  {
+    icon: { testIcon },
+    subTitle: "Sub Title 4",
+    content: "Content 4",
+  },
+  {
+    icon: { testIcon },
+    subTitle: "Sub Title 5",
+    content: "Content 5",
+  },
+];
+
+export default items;
