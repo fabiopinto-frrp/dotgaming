d9acd403a868fcae7b1c48d85ee0e59668661ede
fabiopinto-frrp <fabiopinto.epbjc@gmail.com>
2024-09-14 14:55:57 +0100

    fix: scrollUp smooth

--- frontend/src/Components/Buttons/ScrollUpButton.jsx
+++ frontend/src/Components/Buttons/ScrollUpButton.jsx
@@ -1,7 +1,13 @@
 import { FaArrowUp } from "react-icons/fa";
 import { ScrollUpBtn, ScrollUpButtonContainer } from "./ButtonElements";
+import smoothscroll from "smoothscroll-polyfill";
+import { useEffect } from "react";
 
 const ScrollUpButton = () => {
+  useEffect(() => {
+    smoothscroll.polyfill();
+  }, []);
+
   return (
     <>
       <ScrollUpButtonContainer>
