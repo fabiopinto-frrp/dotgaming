2c2886d96bf81f9a31f44fb82f6a0bce0c24ade4
fabiopinto-frrp <fabiopinto.epbjc@gmail.com>
2024-09-14 15:50:50 +0100

    fix: Changed ScrollUp Behavior

--- frontend/src/Components/Buttons/ScrollUpButton.jsx
+++ frontend/src/Components/Buttons/ScrollUpButton.jsx
@@ -1,36 +1,39 @@
 import { FaArrowUp } from "react-icons/fa";
 import { ScrollUpBtn, ScrollUpButtonContainer } from "./ButtonElements";
 import smoothscroll from "smoothscroll-polyfill";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 
 const ScrollUpButton = () => {
-  useEffect(() => {
-    smoothscroll.polyfill();
-  }, []);
+  const [isVisible, setIsVisible] = useState(false);
 
   const scrollToTop = () => {
-    if ("scrollBehavior" in document.documentElement.style) {
-      window.scrollTo({ top: 0, behavior: "smooth" });
-    } else {
-      let scrollStep = -window.scrollY / (500 / 15),
-        scrollInterval = setInterval(() => {
-          if (window.scrollY !== 0) {
-            window.scrollBy(0, scrollStep);
-          } else clearInterval(scrollInterval);
-        }, 15);
-    }
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
   };
+
+  useEffect(() => {
+    smoothscroll.polyfill();
+    const toggleVisibility = () => {
+      if (window.scrollY > 500) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+    window.addEventListener("scroll", toggleVisibility);
+    return () => window.removeEventListener("scroll", toggleVisibility);
+  }, []);
 
   return (
     <>
       <ScrollUpButtonContainer>
-        <ScrollUpBtn
-          onClick={() => {
-            scrollToTop();
-          }}
-        >
-          <FaArrowUp />
-        </ScrollUpBtn>
+        {isVisible && (
+          <ScrollUpBtn onClick={scrollToTop}>
+            <FaArrowUp />
+          </ScrollUpBtn>
+        )}
       </ScrollUpButtonContainer>
     </>
   );
