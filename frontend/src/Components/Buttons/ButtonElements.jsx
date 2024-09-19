3bcbb3698d7bc4a8a4cf1211d5feecb565dd3b29
fabiopinto-frrp <fabiopinto.epbjc@gmail.com>
2024-09-19 12:16:50 +0100

    fix: PortfolioRedirectButton Styling

--- frontend/src/Components/Buttons/ButtonElements.jsx
+++ frontend/src/Components/Buttons/ButtonElements.jsx
@@ -35,7 +35,7 @@ export const PortfolioRedirectButtonContainer = styled.div`
 
 export const PortfolioRedirectBtn = styled(Link)`
   position: relative;
-  background-color: #400e39;
+  background-color: #420d3b;
   color: #fff;
   border: 2px solid #b502b5;
   border-radius: 1rem;
@@ -46,28 +46,12 @@ export const PortfolioRedirectBtn = styled(Link)`
   overflow: hidden;
   text-decoration: none;
   user-select: none;
-  transition: all 0.3s ease-in-out;
+  transition: all 0.2s ease-in-out;
 
   &:hover {
     color: #fff;
-    border: 2px solid #ac2599;
+    border: 2px solid #ff00ff;
     transform: scale(1.05);
-  }
-
-  &:before {
-    content: "";
-    position: absolute;
-    top: 0;
-    left: -100%;
-    width: 200%;
-    height: 100%;
-    background-color: #ac2599;
-    transform: skewX(-45deg);
-    transition: all 0.3s ease-in-out;
-    z-index: -1;
-  }
-
-  &:hover:before {
-    left: 100%;
+    box-shadow: 0 0 10px #ff00ff;
   }
 `;
